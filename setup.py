c3331dc39c0d7ed852daa0ad51fa60a23a3c233a
iheb <ihebmbarek05@gmail.com>
2024-12-16 05:33:48 -0800

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="wasteDetection",
+    version='0.0.0',
+    author="iheb mbarek",
+    author_email="ihebmbarek05@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
